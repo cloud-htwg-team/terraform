bb6d007664acd1a8ebbf501b20d263148140898c
SkinnyLemon <43953592+SkinnyLemon@users.noreply.github.com>
2023-01-27 16:26:12 +0100

    added basic tenant config

---
+++ terraform-tenant/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {   // TODO move to higher layer?
+  credentials = file("~/.gc/cloud-computing-pre-5df7ea9a7c2c.json")
+
+  project = "qrcode-374515"
+  region  = "europe-west3"
+  zone    = "europe-west3-a"
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "qrcode-history"
+  location      = "europe-west3"
+  storage_class = "REGIONAL"
+  force_destroy = true
+}
